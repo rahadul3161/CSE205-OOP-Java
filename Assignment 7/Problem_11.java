d26e131126217de1d8c6bca03d1918777a68b727
MD RAHADUL ISLAM <riyadrahadulislam3@gmail.com>
2024-12-19 23:32:31 +0600

    Delete Assignment 7 directory

--- Assignment 7/Problem_11.java
+++
@@ -1,9 +0,0 @@
-//Write a Java program to convert miles to kilometers.
-
-public class Rahadul_11 {
-  public static void main(String[] args) {
-    double mile = 15.0;
-    double kilo = mile*1.60934;
-    System.out.println(mile+ " miles is equal to " +kilo + " Kilometers.");
-  }
-}
