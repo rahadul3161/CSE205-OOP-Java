8ae1e7832c0ee63de2cc3934303602c1b8b1ee86
MD RAHADUL ISLAM <riyadrahadulislam3@gmail.com>
2024-12-19 23:30:14 +0600

    Add files via upload

---
+++ Assignment 7/Problem_2.java
@@ -0,0 +1,11 @@
+//Problem-2: Write a Java program to add two numbers.
+
+public class Rahadul_2 {
+    public static void main(String args[]){
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("The sum of a and b value:"+sum);
+    }
+    
+}
---
+++ Assignment 7/Problem_3.java
@@ -0,0 +1,11 @@
+//Problem:- 3, Write a Java program to change temperature from Celsius to Fahrenheit.
+import java.util.*;
+public class temp{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value in Celsius: ");
+        int cel = sc.nextInt();
+        int Fahr = (9*cel/5)+32;
+        System.out.println("Temperature in Fahrenheir: "+Fa
+    }
+}
