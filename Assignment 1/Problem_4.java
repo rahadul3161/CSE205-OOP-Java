c7f411bc6c3d94ff26e79a4c368917e0def0317b
MD RAHADUL ISLAM <riyadrahadulislam3@gmail.com>
2024-12-25 00:57:53 +0600

    Add files via upload

---
+++ Assignment 1/Problem_4.java
@@ -0,0 +1,12 @@
+//Problem:- 4, Write a Java program to change temperature from Fahrenheit to Celsius.
+import java.util.*;
+public class temp{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value in Fahrenheit: ");
+        int fahr = sc.nextInt();
+        int cel = ((fahr-32)*5)/9;
+        System.out.print("In Celsius: "+cel);
+        
+    }
+}
