f61cceda42f756a7b6fcfd622f2bbf85ac473b39
MD RAHADUL ISLAM <riyadrahadulislam3@gmail.com>
2024-12-30 11:20:06 +0600

    Add files via upload

---
+++ Java Assignments/Assignment 1/Problem_14.java
@@ -0,0 +1,14 @@
+//Write a Java program to check whether a number is divisible by 5 or not.
+
+public class Rahadul_14 {
+    public static void main(String args[]) {
+        int num=54;
+        
+        if (num%5==0) {
+            System.out.println(num+" is divisible by 5.");
+        } 
+        else {
+            System.out.println(num+" is not divisible by 5.");
+        }
+    }
+}
---
+++ Java Assignments/Assignment 1/Problem_7.java
@@ -0,0 +1,12 @@
+//Write a Java Program to display whether a number is odd or even.
+
+public class Rahadul_7 {
+  public static void main(String[] args){
+    int num=15;
+    if (num%2==0) {
+      System.out.println(num+ " is even number.");}
+    else {
+      System.out.println(num+ " is odd number.");
+    }
+  }
+}
